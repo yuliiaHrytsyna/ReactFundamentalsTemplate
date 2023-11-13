58ca4d5d39c7a3383ded1973f99a9223a9917341
Olesia_Miller@epam,con <3010Dancer1@>
2023-11-13 12:50:07 +0200

    Added instructions for CourseInfo and Courses components

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 import styles from './styles.module.css';
 
+// Module 2
+// Remove 'handleShowCourse' prop and use 'courseId' and react-router to show CourseInfo page
+
+// Module 3
+// Remove 'coursesList' and 'authorsList' props and use data from store instead
+
 export const Courses = ({coursesList, authorsList, handleShowCourse}) => {
 
 	// write your code here
